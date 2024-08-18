437e815cce54e942bdd1c363ea6107fda557ced5
Pedro <Pedrwsxd@gmail.com>
2024-08-18 16:31:04 -0300

    Correção de Bugs, melhora de reponsividade, adição de animações.

--- assets/js/scrollbar.js
+++
@@ -1,9 +0,0 @@
-export function scrollUp() {
-    const container = document.querySelector('.scroll-content');
-    container.scrollBy({ top: -100, behavior: 'smooth' });
-}
-
-export function scrollDown() {
-    const container = document.querySelector('.scroll-content');
-    container.scrollBy({ top: 100, behavior: 'smooth' });
-}
